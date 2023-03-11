bb1b9eec0c7b942a4c5dcc9e4124fda524361d58
Aryan-ji <aryanak9163@gmail.com>
2023-03-12 01:11:48 +0530

    commit-2
    Till my previous session I have completed upto delete all
    functionality.In this session I have completed till dark mode
    Looking further to add some more interesting concepts in this app

--- static/js/test.js
+++ static/js/test.js
@@ -1,1 +1,29 @@
-console.log("hello flusky");
+const modeBtn = document.getElementById("mode");
+modeBtn.onchange = (e) => {
+  if (modeBtn.checked === true) {
+    document.documentElement.classList.remove("light");
+    document.documentElement.classList.add("dark");
+    document.getElementById("navbar").setAttribute("data-bs-theme", "dark");
+    window.localStorage.setItem("mode", "dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+    document.documentElement.classList.add("light");
+    document.getElementById("navbar").removeAttribute("data-bs-theme");
+
+    window.localStorage.setItem("mode", "light");
+  }
+};
+
+const mode = window.localStorage.getItem("mode");
+if (mode == "dark") {
+  modeBtn.checked = true;
+  document.documentElement.classList.remove("light");
+  document.documentElement.classList.add("dark");
+  document.getElementById("navbar").setAttribute("data-bs-theme", "dark");
+}
+if (mode == "light") {
+  modeBtn.checked = false;
+  document.documentElement.classList.remove("dark");
+  document.documentElement.classList.add("light");
+  document.getElementById("navbar").removeAttribute("data-bs-theme");
+}
